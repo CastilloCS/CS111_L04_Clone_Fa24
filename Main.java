9ebcd0437cc3a27654ca4673a87f6e6655a02122
Stephen Cerruti <scerruti@miracosta.edu>
2024-08-24 07:48:55 -0700

    FA24 Update
    Added solution
    Corrected test name

--- Main.java
+++ Main.java
@@ -1,22 +1,34 @@
-// YOUR NAME: 
-// COLLABORATORS:  
-// DATE: 
+// YOUR NAME:
+// DATE:
 
 public class Main
 {
 	public static void main(String[] args)
 	{
-		// DECLARATION SECTION
+		//DECLARATION + INITIALIZATION SECTION
+		double avgDacaTaxContribution;
 
-		// INITIALIZATION SECTION
-		
-		// INPUT SECTION
-		// N/A (no input for this lab)
-		
-		// CALCULATION & PROCESSING SECTION
+		char stateMostDaca1 = 'c';
+		char stateMostDaca2 = 'a';
 
-		
-		// OUTPUT SECTION
-		
+		int numDacaRecipients = 700000;
+		long dacaFederalTaxRevenue = 5700000000L;
+
+		//INPUT SECTION
+		// N/A
+
+		//CALCULATION SECTION
+		avgDacaTaxContribution = dacaFederalTaxRevenue / (double) numDacaRecipients;
+		stateMostDaca1 -= 32;
+		stateMostDaca2 -= 32;
+
+		//OUTPUT SECTION
+		System.out.println("Average DACA recipient tax contribution: $" + avgDacaTaxContribution);
+
+//		System.out.println("Average DACA recipient tax contribution: $" + ((int)(100*avgDacaTaxContribution))/100.0);
+
+		System.out.println("Average DACA recipient tax contribution, rounded down to the nearest whole dollar: $" + (int) avgDacaTaxContribution);
+
+		System.out.println("State with the most DACA recipients: " + stateMostDaca1 + stateMostDaca2);
 	}
 }
